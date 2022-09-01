f03c8e34b86b47d4828f6a7a562734fb9bd0cea2
CYBYOB <1520112971@qq.com>
2022-09-01 17:28:56 +0800

    P_3：attr、classed、style、property、text、html 等函数的简单入门

--- project/react-ts-hook/src/components/MyD3/Practice/index.tsx
+++ project/react-ts-hook/src/components/MyD3/Practice/index.tsx
@@ -1,6 +1,52 @@
 import * as d3 from 'd3';
 import { useCallback, useEffect } from "react"
+import './index.less';
 
+// P_3：attr、classed、style、property、text、html 等函数的简单入门
+export function P_3() {
+    const draw = useCallback(() => {
+        // const svg = d3.select("#P_3").append('svg').attr("width", 1000).attr("height", 1000);
+        const el = d3.select("#P_3");
+        // 批量添加样式 —— class属性
+        el.attr('class', 'class_1 class_2');
+        // 开关 class 属性
+        el.classed('class_1', false)
+
+        // 批量设置样式 —— style对象（TODO 较新版本暂不支持，为哈呢？）
+        el.style('color', 'red')
+            .style('text-align', 'center');
+
+        // 不能用 attr 获取的属性值，可考虑使用 property (如输入框的 value 值)
+       setTimeout(() => {
+            const inputEl = d3.select('#input'),
+                valueAttr = inputEl.attr('value'),
+                valueProperty = inputEl.property('value');
+            // 注： valueAttr 为 null，valueProperty 能取到值。
+
+            const elClassAttr = el.attr('class'),
+                elClassProperty = el.property('class');
+            // 注： valueAttr 能取到值，valueProperty 为 null。
+       }, 3* 1000);
+
+        // .text()：获取或设置选择集的文本，文本内容相当于 DOM的 innerText ，不包含元素内部的标签
+        const text = el.text();
+
+        // .html()：获取或设置选择集的文本，文本内容相当于 DOM的 innerHTML ，包含元素内部的标签
+        const html = el.html();
+        el.html('<p>我是通过 html 函数插入的 p 标签（注：会把之前父元素里面的所有标签给清空、覆盖）</p>');
+    }, []);
+
+    useEffect(() => {
+        draw();
+    }, [draw]);
+
+    return (
+        <div id="P_3">
+            我是P_3
+            <input id='input' type="text" />
+        </div>
+    )
+}
 
 // P_2：画圆
 export function P_2() {
